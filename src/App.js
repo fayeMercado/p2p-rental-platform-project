4b90b63097862d00526662dd847e2e165f8878a0
Kristoff Villanueva <kristoffer.villanueva18@gmail.com>
2022-07-20 22:21:26 +0800

    added signup and login pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
-import HeaderProducts from "./Headers/HeaderProducts";
-import HeaderLanding from "./Headers/HeaderLanding";
-import HeaderMain from "./Headers/HeaderMain";
+// import HeaderProducts from "./Headers/HeaderProducts";
+// import HeaderLanding from "./Headers/HeaderLanding";
+// import HeaderMain from "./Headers/HeaderMain";
+import Signup from "./Signup/Signup";
+import Login from "./Login/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <HeaderLanding />
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* <HeaderLanding /> */}
+			<Signup />
+		</div>
+	);
 }
 
 export default App;
--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -1,12 +1,81 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Signup.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Signup.module.css";
+import close from "../Login/images/Close.png";
 
-const Signup = () => (
-  <div className={styles.Signup} data-testid="Signup">
-    Signup Component
-  </div>
-);
+function Signup() {
+	return (
+		<div className={styles.mainDiv}>
+			<div className={styles.divSignupMain}>
+				<img src={close} className={styles.close} />
+				<h1 className={styles.signupH1}>CREATE A NEW ACCOUNT</h1>
+				<form action="">
+					<div>
+						<label htmlFor="">First Name</label>
+						<input
+							type="text"
+							placeholder="First Name"
+							name="first name"
+							required
+						/>
+						<label htmlFor="">Last Name</label>
+						<input
+							type="text"
+							placeholder="Last Name"
+							name="first name"
+							required
+						/>
+						<label htmlFor="">E-mail</label>
+						<input
+							type="text"
+							placeholder="E-mail"
+							name="first name"
+							required
+						/>
+						<label htmlFor="">Phone Number</label>
+						<input
+							type="text"
+							placeholder="+639XXXXXXXXX"
+							name="first name"
+							required
+						/>
+						<label htmlFor="">Password</label>
+						<input
+							type="password"
+							placeholder="Password"
+							name="first name"
+							required
+						/>
+						<label htmlFor="">Confirm Password</label>
+						<input
+							type="password"
+							placeholder="Confirm Password"
+							name="first name"
+							required
+						/>
+						<div className="divTerms">
+							<input
+								className={styles.checkBox}
+								type="checkbox"
+								placeholder="Password"
+								name="first name"
+								required
+							/>
+							<label className={styles.checkboxText} htmlFor="">
+								I agree to the Handeeman's Terms of Use including the
+								collection, use and disclosure of my personal data in accordance
+								with and for the purposes set out in the Privacy Policy
+							</label>
+						</div>
+						<div className={styles.divRegister}>
+							<button className={styles.buttonRegister}>Register</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
 
 Signup.propTypes = {};
 
