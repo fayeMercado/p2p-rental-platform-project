cae91d4c763d1f8191a96feadc338cd048c4ebe9
MFMercado <ar.mflmercado@gmail.com>
2022-07-21 23:22:23 +0800

    updated product page

--- src/ProductPage/ProductList.js
+++ src/ProductPage/ProductList.js
@@ -6,13 +6,6 @@ import { ProductCard } from "./ProductCard";
 export function ProductList() {
   return (
     <Container className="p-0">
-      <Container
-        fluid
-        className="d-flex mb-3 p-0 align-items-end justify-content-between"
-      >
-        <h3 className="fontMain fw-bold m-0">COMPUTER & ACCESSORIES</h3>
-        <span>0 products found.</span>
-      </Container>
       <Row className="g-3">
         {ProductCard()}
         {ProductCard()}
