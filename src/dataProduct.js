49f26bf9bdadeb60bad1d994768efb77ddb578e7
MFMercado <ar.mflmercado@gmail.com>
2022-07-21 23:20:28 +0800

    updated files

---
+++ src/dataProduct.js
@@ -0,0 +1,10 @@
+export const Categories = [
+  "All Categories",
+  "Electronics",
+  "Computer & Accessories",
+  "Tools & Equipment",
+  "Home Furniture & Appliances",
+  "Party & Event Items",
+  "Toys & Sports",
+  "Books & Hobbies",
+];
