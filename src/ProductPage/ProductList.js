d800656955425ce3dea66e9ca183705a3f907034
MFMercado <ar.mflmercado@gmail.com>
2022-07-29 18:42:19 +0800

    updates on button

--- src/ProductPage/ProductList.js
+++ src/ProductPage/ProductList.js
@@ -5,29 +5,39 @@ import Col from "react-bootstrap/Col";
 import { ProductCard } from "./ProductCard";
 import { getProducts } from "../dataProduct";
 import { useParams } from "react-router-dom";
+import { getCategory } from "../dataCategories";
 
 export function ProductList() {
   let products = getProducts();
   const [filtered, setFiltered] = useState([]);
   const urlparams = useParams().category;
+  const category = getCategory(urlparams);
   useEffect(() => {
     let filterCurrProduct = products.filter(
       (item) => item.category === urlparams
     );
     setFiltered(filterCurrProduct);
   }, [urlparams]);
+  const productList = useParams().category === "all" ? products : filtered;
 
   return (
     <Container className="p-0">
+      <Container
+        fluid
+        className="d-flex mb-3 p-0 align-items-end justify-content-between"
+      >
+        <h3 className="fontMain fw-bold m-0">{category.text.toUpperCase()}</h3>
+        <span>
+          {productList.length} {productList.length > 1 ? "products" : "product"}{" "}
+          found.
+        </span>
+      </Container>
       <Row className="g-3">
-        {/* {useParams().category === "all" ? } */}
-        {(useParams().category === "all" ? products : filtered).map(
-          (product, i) => (
-            <Col xs={3} key={i}>
-              <ProductCard itemCode={product} />
-            </Col>
-          )
-        )}
+        {productList.map((product, i) => (
+          <Col xs={3} key={i}>
+            <ProductCard itemCode={product} />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
