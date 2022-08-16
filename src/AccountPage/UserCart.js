dfbbda08ff48ac00c2884a1423e3831676523d1d
MFMercado <ar.mflmercado@gmail.com>
2022-08-17 02:08:27 +0800

    updated fetch trial

--- src/AccountPage/UserCart.js
+++ src/AccountPage/UserCart.js
@@ -96,7 +96,7 @@ export function UserCart() {
       body: JSON.stringify(itemToBeDeleted),
     };
 
-    fetch("http://phplaravel-821102-2821130.cloudwaysapps.com/cart", requestOptions)
+    fetch("https://phplaravel-821102-2821130.cloudwaysapps.com/cart", requestOptions)
       .then((response) => response.json())
       .then((result) => {
         const filteredCart = result.filter(
@@ -145,7 +145,7 @@ export function UserCart() {
         body: JSON.stringify(toCheckout),
       };
 
-      fetch("http://phplaravel-821102-2821130.cloudwaysapps.com/cart/checkout", requestOptions)
+      fetch("https://phplaravel-821102-2821130.cloudwaysapps.com/cart/checkout", requestOptions)
         .then((response) => response.json())
         .then((result) => {
           console.log("result", result);
--- src/Profile/Modal.js
+++ src/Profile/Modal.js
@@ -1,268 +1,264 @@
 import React, { useState } from "react";
-import { useEffect } from "react"
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Modal, Form, Row, Col } from "react-bootstrap";
 import { AppButtonYellow, AppButtonGreen } from "../CustomComponents/AppButton";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
 
 export function Signup(props) {
-    const provinces = require("philippines/provinces");
-    const navigate = useNavigate();
-    const cart_id = nanoid(10);
+  const provinces = require("philippines/provinces");
+  const navigate = useNavigate();
+  const cart_id = nanoid(10);
 
-    // const [cart_id, setCartId] = useState("");
-    const [firstname, setFirstName] = useState("");
-    const [lastname, setLastName] = useState("");
-    const [address, setAddress] = useState("");
-    const [mobile, setMobile] = useState("");
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  // const [cart_id, setCartId] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  async function signUp(event) {
+    let item = {
+      cart_id,
+      firstname,
+      lastname,
+      address,
+      mobile,
+      username,
+      email,
+      password,
+    };
+    event.preventDefault();
+    setFirstName("");
+    setLastName("");
+    setAddress("");
+    setMobile("");
+    setUsername("");
+    setEmail("");
+    setPassword("");
 
+    let result = await fetch(
+      "https://phplaravel-821102-2821130.cloudwaysapps.com/register",
+      {
+        method: "POST",
+        body: JSON.stringify(item),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+    result = await result.json();
+    navigate("/");
+  }
 
-    async function signUp(event) {
-
-        let item = { cart_id, firstname, lastname, address, mobile, username, email, password };
-        event.preventDefault();
-        setFirstName("");
-        setLastName("");
-        setAddress("");
-        setMobile("");
-        setUsername("");
-        setEmail("");
-        setPassword("");
-
-        let result = await fetch("http://phplaravel-821102-2821130.cloudwaysapps.com/register", {
-            method: 'POST',
-            body: JSON.stringify(item),
-            headers: {
-                "Content-Type": 'application/json',
-                "Accept": 'application/json'
-            }
-        })
-        result = await result.json()
-        navigate("/");
-    }
-
-    return (
-        <Modal
-            {...props}
-            size="md"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            style={{ backgroundColor: 'rgba(0,0,0, 0.7)' }}
-        >
-            <Modal.Header closeButton>
-                <Modal.Title>REGISTER A NEW ACCOUNT</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={signUp}>
-
-                    <Row>
-                        <Col xs={12} md={6}>
-                            <Form.Group className="mb-3" >
-                                <Form.Label>First Name</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="First Name"
-                                    value={firstname}
-                                    onChange={(e) => setFirstName(e.target.value)}
-                                    autoFocus
-                                />
-                            </Form.Group>
-                        </Col>
-                        <Col xs={6} md={6}>
-                            <Form.Group className="mb-3" >
-                                <Form.Label>Last Name</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Last Name"
-                                    value={lastname}
-                                    onChange={(e) => setLastName(e.target.value)}
-                                    autoFocus
-                                />
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Address</Form.Label>
-                        <Form.Select
-                            aria-label="Default select example"
-                            value={address}
-                            onChange={(e) => setAddress(e.target.value)}
-                        >
-                            <option>Select Address</option>
-                            {provinces.map((province) => (
-                                <option key={province.key} value={province.key}>
-                                    {province.name}
-                                </option>
-                            ))}
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Mobile Number</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="09XXXXXXXXX"
-                            value={mobile}
-                            onChange={(e) => setMobile(e.target.value)}
-                            autoFocus
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            autoFocus
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            type="email"
-                            placeholder="name@example.com"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            autoFocus
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            autoComplete="on"
-                            autoFocus
-                        />
-                    </Form.Group>
-                    <AppButtonGreen
-                        type="submit">
-                        Register
-                    </AppButtonGreen>
-                </Form>
-            </Modal.Body>
-            {/* <Modal.Footer>
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      style={{ backgroundColor: "rgba(0,0,0, 0.7)" }}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>REGISTER A NEW ACCOUNT</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={signUp}>
+          <Row>
+            <Col xs={12} md={6}>
+              <Form.Group className="mb-3">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="First Name"
+                  value={firstname}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  autoFocus
+                />
+              </Form.Group>
+            </Col>
+            <Col xs={6} md={6}>
+              <Form.Group className="mb-3">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Last Name"
+                  value={lastname}
+                  onChange={(e) => setLastName(e.target.value)}
+                  autoFocus
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Form.Group className="mb-3">
+            <Form.Label>Address</Form.Label>
+            <Form.Select
+              aria-label="Default select example"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            >
+              <option>Select Address</option>
+              {provinces.map((province) => (
+                <option key={province.key} value={province.key}>
+                  {province.name}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Mobile Number</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="09XXXXXXXXX"
+              value={mobile}
+              onChange={(e) => setMobile(e.target.value)}
+              autoFocus
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              autoFocus
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoFocus
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="on"
+              autoFocus
+            />
+          </Form.Group>
+          <AppButtonGreen type="submit">Register</AppButtonGreen>
+        </Form>
+      </Modal.Body>
+      {/* <Modal.Footer>
                 <AppButtonGreen
                     type="button"
                     onClick={props.onHide}>
                     Register
                 </AppButtonGreen>
             </Modal.Footer> */}
-        </Modal >
-    );
+    </Modal>
+  );
 }
 
-
-
-
 export function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+  // const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     console.log(email, password);
+  //     setEmail("");
+  //     setPassword("");
+  // };
 
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     console.log(email, password);
-    //     setEmail("");
-    //     setPassword("");
-    // };
+  async function logIn(event) {
+    console.log(email, password);
+    event.preventDefault();
+    setEmail("");
+    setPassword("");
 
-    async function logIn(event) {
-        console.log(email, password);
-        event.preventDefault();
-        setEmail("");
-        setPassword("");
+    let item = { email, password };
+    let result = await fetch(
+      "https://phplaravel-821102-2821130.cloudwaysapps.com/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(item),
+      }
+    );
+    result = await result.json();
+    localStorage.setItem("user-info", JSON.stringify(result));
+    navigate("/products/all");
+  }
 
-        let item = { email, password }
-        let result = await fetch("http://phplaravel-821102-2821130.cloudwaysapps.com/login", {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(item)
-        });
-        result = await result.json()
-        localStorage.setItem('user-info', JSON.stringify(result))
-        navigate("/products/all")
-    }
+  // const [loginInfo, setLoginInfo] = useState({
+  //     email: "",
+  //     password: "",
+  // });
 
-    // const [loginInfo, setLoginInfo] = useState({
-    //     email: "",
-    //     password: "",
-    // });
+  // const handleChange = (event) => {
+  //     setLoginInfo({ ...loginInfo, [event.target.name]: event.target.value });
+  // };
 
-    // const handleChange = (event) => {
-    //     setLoginInfo({ ...loginInfo, [event.target.name]: event.target.value });
-    // };
+  // const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     console.log(loginInfo);
+  //     setLoginInfo({ email: "", password: "" });
+  // };
 
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     console.log(loginInfo);
-    //     setLoginInfo({ email: "", password: "" });
-    // };
-
-
-
-
-    return (
-
-        <Modal
-            {...props}
-            size="md"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            style={{ backgroundColor: 'rgba(0,0,0, 0.7)' }}
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">LOG IN</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={logIn}>
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            type="email"
-                            placeholder="name@example.com"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            // value={loginInfo.email}
-                            // name="email"
-                            // onChange={handleChange}
-                            autoFocus
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            // value={loginInfo.password}
-                            // name="password"
-                            // onChange={handleChange}
-                            autoComplete="on"
-                            autoFocus
-                            required
-                        />
-                    </Form.Group>
-                    <AppButtonYellow
-                        type="submit"
-                    >
-                        Login
-                    </AppButtonYellow>
-                </Form>
-            </Modal.Body>
-            {/* <span
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      style={{ backgroundColor: "rgba(0,0,0, 0.7)" }}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">LOG IN</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={logIn}>
+          <Form.Group className="mb-3">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              // value={loginInfo.email}
+              // name="email"
+              // onChange={handleChange}
+              autoFocus
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              // value={loginInfo.password}
+              // name="password"
+              // onChange={handleChange}
+              autoComplete="on"
+              autoFocus
+              required
+            />
+          </Form.Group>
+          <AppButtonYellow type="submit">Login</AppButtonYellow>
+        </Form>
+      </Modal.Body>
+      {/* <span
                 style={{
                     fontWeight: '700',
                     fontSize: '12px',
@@ -278,6 +274,6 @@ export function Login(props) {
                     Login
                 </AppButtonYellow>
             </Modal.Footer> */}
-        </Modal>
-    );
+    </Modal>
+  );
 }
